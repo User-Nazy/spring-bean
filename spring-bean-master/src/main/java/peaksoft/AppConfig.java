7815b5f447a54b5c0acb6a88fa6f746fd366d4b3
Nazgul Kubanychbekova <kubanazgul88@gmail.com>
2021-07-17 14:52:07 +0300

    Initial commit

---
+++ spring-bean-master/src/main/java/peaksoft/AppConfig.java
@@ -0,0 +1,21 @@
+package peaksoft;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Cat catBean(){
+        return new Cat();
+    }
+}
